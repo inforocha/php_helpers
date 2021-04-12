0acaf4a5e8e00dd83c4961db5d87c87a4c557e80
Davi Samuel de O. Rocha <info.rocha2@gmail.com>
2021-04-12 09:01:55 -0300

    uuid helper

---
+++ uuid_helper.php
@@ -0,0 +1,7 @@
+<?php
+if(!function_exists(uuid_v4)) {
+	function uuid_v4() {
+		$uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+		return $uuid;
+	}
+}
